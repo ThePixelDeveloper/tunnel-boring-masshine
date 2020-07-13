55d2d9a8190a92efc811480eb5a7254ea8b5ef57
Mathew Davies <thepixeldeveloper@icloud.com>
2020-07-13 23:46:50 +0100

    feat: create a sidebar component and link it up to the vuex state

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, {mapState} from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tunnels: [{
+      name: "Nomad",
+      status: "Connected"
+    }, {
+      name: "Consul",
+      status: "Connecting"
+    }, {
+      name: "Traefik",
+      status: "Disconnected"
+    }],
   },
   mutations: {
+    addTunnel (state) {
+      state.tunnels.push({
+        name: "Tunnel",
+        status: "Connected"
+      })
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  computed: mapState([
+    "tunnels"
+  ])
 })
