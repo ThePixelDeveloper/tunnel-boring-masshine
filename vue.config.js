07768206d10ba3415f770f15b110de7adcb13b5d
Mathew Davies <ThePixelDeveloper@users.noreply.github.com>
2020-10-04 22:43:18 +0100

    fix: incorrect placement of the electron builder properties

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,15 @@ module.exports = {
     electronBuilder: {
       nodeIntegration: false,
       preload: "src/preload.js",
+      builderOptions: {
+        appId: "com.thepixeldeveloper.tunnelboringmasshine",
+        afterSign: "electron-builder-notarize",
+        mac: {
+          hardenedRuntime: true,
+          entitlements:
+            "./node_modules/electron-builder-notarize/entitlements.mac.inherit.plist",
+        },
+      },
     },
   },
   pages: {
