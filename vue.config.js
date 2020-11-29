f92a5c47c1fc5e2c126b950514f2448fb19345d5
Mathew Davies <ThePixelDeveloper@users.noreply.github.com>
2020-11-29 22:31:05 +0000

    docs: bits and pieces

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,12 @@ module.exports = {
       preload: "src/preload.js",
       builderOptions: {
         appId: "com.thepixeldeveloper.tunnelboringmasshine",
+        productName: "Tunnel Boring MaSSHine",
+        copyright: "Copyright © 2020 Mathew Davies",
         afterSign: "electron-builder-notarize",
         mac: {
           hardenedRuntime: true,
+          category: "public.app-category.developer-tools",
           entitlements:
             "./node_modules/electron-builder-notarize/entitlements.mac.inherit.plist",
         },
